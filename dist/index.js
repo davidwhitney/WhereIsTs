46fcf5482e0873782dd3c38bf13b07e8ce2e5e79
David Whitney <david@davidwhitney.co.uk>
2020-02-26 13:58:20 +0000

    compiled new build

--- dist/index.js
+++ dist/index.js
@@ -1,12 +1,27 @@
 "use strict";
-/**
- * Responds to any HTTP request.
- *
- * @param {!express:Request} req HTTP request context.
- * @param {!express:Response} res HTTP response context.
- */
-exports.helloWorld = (req, res) => {
+Object.defineProperty(exports, "__esModule", { value: true });
+const WhereIsCommand_1 = require("./WhereIsCommand");
+const LocationFinder_1 = require("./FindingPlaces/LocationFinder");
+const LocationCollection_1 = require("./FindingPlaces/LocationCollection");
+const UrlHelper_1 = require("./Infrastructure/UrlHelper");
+var fs = require('fs');
+const configuration = {
+    UrlRoot: "https://localhost/api",
+    ApiKey: "key123",
+    CapacityApiKey: "",
+    BlobCredentials: "",
+    Root: "",
+    MapPath: ""
+};
+const knownLocationsText = fs.readFileSync("./../App_Data/locations.json");
+const knownLocations = JSON.parse(knownLocationsText);
+const locations = new LocationCollection_1.LocationCollection(...knownLocations);
+const locationFinder = new LocationFinder_1.LocationFinder(locations);
+const urlHelper = new UrlHelper_1.UrlHelper(configuration);
+exports.whereIsCommand = (req, res) => {
     let message = req.query.message || req.body.message || 'Hello World!';
-    res.status(200).send("this definitely came from my code because it started in typescript");
+    const command = new WhereIsCommand_1.WhereIsCommand(locationFinder, urlHelper);
+    const result = command.execute(req);
+    res.status(200).send(result);
 };
 //# sourceMappingURL=index.js.map
