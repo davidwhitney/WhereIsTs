4d0e3b13953b680d03622668d4a120f446e88976
David Whitney <david@davidwhitney.co.uk>
2020-02-26 13:36:37 +0000

    Cloud function test wireup.

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,30 @@
-/**
- * Responds to any HTTP request.
- *
- * @param {!express:Request} req HTTP request context.
- * @param {!express:Response} res HTTP response context.
- */
-exports.helloWorld = (req, res) => {
+import { WhereIsCommand } from "./WhereIsCommand";
+import { LocationFinder } from "./FindingPlaces/LocationFinder";
+import { LocationCollection } from "./FindingPlaces/LocationCollection";
+import { UrlHelper } from "./Infrastructure/UrlHelper";
+import { Configuration } from "./Infrastructure/Configuration";
+var fs = require('fs');
+
+const configuration: Configuration = { 
+  UrlRoot: "https://localhost/api", 
+  ApiKey: "key123", 
+  CapacityApiKey: "", 
+  BlobCredentials: "", 
+  Root: "", 
+  MapPath: ""
+};
+
+const knownLocationsText = fs.readFileSync("./../App_Data/locations.json");
+const knownLocations = JSON.parse(knownLocationsText);
+const locations = new LocationCollection(...knownLocations);
+const locationFinder = new LocationFinder(locations);
+const urlHelper = new UrlHelper(configuration);
+
+exports.whereIsCommand = (req, res) => {
     let message = req.query.message || req.body.message || 'Hello World!';
-    res.status(200).send("this definitely came from my code because it started in typescript");
+    const command = new WhereIsCommand(locationFinder, urlHelper);
+    const result = command.execute(req);    
+
+    res.status(200).send(result);
   };
   
