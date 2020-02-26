5987e038346d655e9088ba886d2e4fe127f7705c
David Whitney <david@davidwhitney.co.uk>
2020-02-26 16:09:38 +0000

    Older module export style for google?

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,26 @@
 import { capacity, checkin, heatmap, map, whereis } from "./AppFactory";
-/*
-export const capacityCommand = async (req, res) => {
+
+module.exports.capacityCommand = async (req, res) => {
   const result = await capacity.execute(req);  
   res.send(result);
 };
 
-export const checkinCommand = async (req, res) => {
+module.exports.checkinCommand = async (req, res) => {
   const result = await checkin.execute(req);  
   res.send(result);
 };
 
-export const heatmapCommand = async (req, res) => {
+module.exports.heatmapCommand = async (req, res) => {
   const result = await heatmap.execute(req);  
   res.send(result);
 };
 
-export const mapCommand = async (req, res) => {
+module.exports.mapCommand = async (req, res) => {
   const result = await map.execute(req);  
   res.send(result);
-};*/
+};
 
-export const whereisCommand = async (req, res) => {
+module.exports.whereisCommand = async (req, res) => {
   const result = await whereis.execute(req);  
   res.send(result);
 };
