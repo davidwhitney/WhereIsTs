ffa3eee9637d69a211566176bb0eff56a537ea84
David Whitney <david@davidwhitney.co.uk>
2020-02-26 16:18:43 +0000

    Back again.

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,26 @@
 import { capacity, checkin, heatmap, map, whereis } from "./AppFactory";
 
-module.exports.capacityCommand = async (req, res) => {
+export const capacityCommand = async (req, res) => {
   const result = await capacity.execute(req);  
   res.send(result);
 };
 
-module.exports.checkinCommand = async (req, res) => {
+export const checkinCommand = async (req, res) => {
   const result = await checkin.execute(req);  
   res.send(result);
 };
 
-module.exports.heatmapCommand = async (req, res) => {
+export const heatmapCommand = async (req, res) => {
   const result = await heatmap.execute(req);  
   res.send(result);
 };
 
-module.exports.mapCommand = async (req, res) => {
+export const mapCommand = async (req, res) => {
   const result = await map.execute(req);  
   res.send(result);
 };
 
-module.exports.whereisCommand = async (req, res) => {
+export const whereisCommand = async (req, res) => {
   const result = await whereis.execute(req);  
   res.send(result);
 };
